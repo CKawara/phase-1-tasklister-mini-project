ff2872202286356f7c6731d988ac3d484ccb4d03
Catherine Kawara <ndutakawara2@gmail.com>
2022-04-15 13:11:41 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const li = document.createElement('li')
+    li.textContent = e.target.new_task_description.value
+
+    const ul = document.getElementById('tasks')
+    ul.appendChild(li)
+
+    form.reset()
+
+  })
 });
